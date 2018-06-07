f834152e3336e97f55da0ffb5cc1e7be9917ebd1
Florian Boemmel <florian.boemmel@st.oth-regensburg.de>
2018-06-07 10:33:50 +0200

    Create new branch

---
+++ BSW_I2C/RTE_LED.c
@@ -0,0 +1,34 @@
+#include "RTE_LED.h"
+
+
+void RTE_DIO_READ_DATA(){
+
+    Dio_Interface(READ);
+
+}
+
+void RTE_DIO_WRITE_DATA(DIO_Devices device, DATA_TO_SEND option){
+
+    Dio_Interface(WRITE, device, option);
+}
+
+void Dio_Interface(uint8_t ReadOrWrite, DIO_Devices device, DATA_TO_SEND option){
+
+    if(ReadOrWrite){
+
+
+        #ifdef EXTERN_DIO
+
+
+        #endif // EXTERN_DIO
+
+
+        #ifdef INTERN_DIO
+
+        #endif // INTERN_DIO
+
+    }else{
+
+    }
+
+}
---
+++ BSW_I2C/RTE_LED.h
@@ -0,0 +1,21 @@
+#ifndef RTE_LED_H_INCLUDED
+#define RTE_LED_H_INCLUDED
+
+#include <stdint.h>
+
+#define EXTERN_DIO
+#define INTERN_DIO
+
+#define WRITE 1
+#define READ 0
+
+typedef enum {LED} DIO_Devices;
+typedef enum {LEFT,RIGHT,ALL} DATA_TO_SEND;
+
+void RTE_DIO_READ_DATA();
+void RTE_DIO_WRITE_DATA(DIO_Devices device, DATA_TO_SEND option);
+
+void Dio_Interface(uint8_t ReadOrWrite, DIO_Devices device, DATA_TO_SEND option);
+
+
+#endif // RTE_LED_H_INCLUDED
