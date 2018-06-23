e28a513de3608bbd1c216af179d2334063546815
SimH1993 <github@hoelzl-server.de>
2018-06-23 10:36:33 +0200

    updated

--- bsw/DIO/DioIF.c
+++ bsw/DIO/DioIF.c
@@ -2,24 +2,18 @@
 #include "i2c.h"
 
 U8 dio_read_int(U8 port_id, U8 i2c address){
-	return return 
+	return ecrobot_get_touch_sensor(port_id);
 }
 
 U8 dio_read_ext(U8 port_id, U8 i2c address){
-	
-	
 	return i2c_read(port_id, i2c_address);
 }
 
 void dio_write_int(U8 port_id, U8 i2c address, U8 data){
 	
-	
-	
 }
 
 void dio_write_ext(U8 port_id, U8 i2c address, U8 data){
-	
-	
 	i2c_write(port_id, i2c_address, data);
 }
 
