b5d416ff167a77f019bb840aba2b323ddea12a3c
Joshua <Joshua.Parhammer@Formulastudent-Regensburg.de>
2018-06-24 22:06:13 +0200

    Adapted the AdcIF to the i2c interface

--- bsw/ADC/AdcIF.c
+++ bsw/ADC/AdcIF.c
@@ -1,17 +1,26 @@
 #include "AdcIf.h"
 #include "ecrobot.h"
-#include "i2c_ext.h"
+#include "i2c.h"
 
 #define CONTROLBYTE (0x04)
 
 const adc_read_fct_t AdcIfFctPtr={adc_int, adc_ext};
 
 S16 adc_int(U8 port_id, U8 address){
-	return ecrobot_get_RCX_sensor(port)>>2; //convert to 8 bit value
+	//return ecrobot_get_RCX_sensor(port)>>2; //convert to 8 bit value
+	// Make the init call of the i2c
+	return 0;
 }
 	
 S16 adc_ext(U8 port_id, U8 address){
 	U8 data[5];
-	ecrobot_send_i2c(NXT_PORT_S4, address, CONTROLBYTE, data, 0);
-	ecrobot_read_i2c(NXT_PORT_S4, address, CONTROLBYTE, data, 5); //analog value will be compared while reading previous value
+	// ecrobot_send_i2c(NXT_PORT_S4, address, CONTROLBYTE, data, 0);
+	i2c_write(NXT_PORT_S4, address, data);
+	// ecrobot_read_i2c(NXT_PORT_S4, address, CONTROLBYTE, data, 5); //analog value will be compared while reading previous value
+	data[0] = i2c_read(NXT_PORT_S4, address);
+	data[1] = i2c_read(NXT_PORT_S4, address);
+	data[2] = i2c_read(NXT_PORT_S4, address);
+	data[3] = i2c_read(NXT_PORT_S4, address);
+	data[4] = i2c_read(NXT_PORT_S4, address);	
 	return ((S16)data[port_id + 1]);
+}
