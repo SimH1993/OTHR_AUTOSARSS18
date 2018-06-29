fdf79504bc24159b1563879ba848d05faa6a3609
cynt4k <me@cynt4k.de>
2018-06-29 16:33:59 +0200

    Initial commit for c code generator

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+import java.io.File;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import autosarMetaModel.*;
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put
+			(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+		
+		File file = new File("C:\\Users\\gra44333\\runtime-EclipseApplication\\Test2\\My.autosarmetamodel");
+		URI uri = file.isFile() ? URI.createFileURI(file.getAbsolutePath()) : URI.createURI("AutosarMetaModel");
+		
+		try {
+			Resource resource = resourceSet.getResource(uri, true);
+			System.out.println("Loaded: " + uri);
+		} catch (RuntimeException e) {
+            System.out.println("Problem loading " + uri);
+            e.printStackTrace();
+		}
+	}
+}
