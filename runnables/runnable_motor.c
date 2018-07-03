3ed74c714604e8a28ad50c158bbdbcc8880eaff4
Christoph Koenig <koc31359@hs-regensburg.de>
2018-07-03 11:00:10 +0200

    Added defines in motor runnable

--- runnables/runnable_motor.c
+++ runnables/runnable_motor.c
@@ -1,7 +1,8 @@
 #include "MotorHandler.h"
 
-
-S8 motor_left, motor_right;
+//TODO: @CodeGen: Generate defines for hardware
+//#define MOTOR_PORT_LEFT	NXT_PORT_A
+//#define MOTOR_PORT_RIGHT	NXT_PORT_B
 
 void runnable_motor() {	
 
@@ -11,6 +12,6 @@ void runnable_motor() {
 	S8 motor_right = (S8)((data & 0x0000FF00) >> 8);
 
 
-	motor_set_speed(motor_left, motor_right);	
+	motor_set_speed(MOTOR_PORT_LEFT, MOTOR_PORT_RIGHT, motor_left, motor_right);	
 	
 }
