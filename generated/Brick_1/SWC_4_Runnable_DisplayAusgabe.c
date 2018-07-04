b6eda65fcb062f9b34196de50a282ed41b4300ec
Joshua Parhammer <Joshua.Parhammer@Formulastudent-Regensburg.de>
2018-07-04 09:22:35 +0200

    small fix

--- generated/Brick_1/SWC_4_Runnable_DisplayAusgabe.c
+++ generated/Brick_1/SWC_4_Runnable_DisplayAusgabe.c
@@ -4,11 +4,11 @@
 void swc_4_DisplayAusgabe(){
    
 	display_goto_xy(0, 0);
-	display_unsigned(((U8)RTE_JOYSTICK_X_READ()),5);
+	display_int(((S8)RTE_JOYSTICK_X_READ()),5);
 	display_goto_xy(0, 1);
-	display_unsigned(((U8)RTE_JOYSTICK_Y_READ()),5);
+	display_int(((S8)RTE_JOYSTICK_Y_READ()),5);
 	display_goto_xy(0, 2);
-	display_unsigned(((U8)RTE_JOYSTICK_BUTTON_READ()),5);
+	display_int(((S8)RTE_JOYSTICK_BUTTON_READ()),5);
 	display_update();
 }
 
