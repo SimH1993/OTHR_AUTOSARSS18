08e2f265c0e590099b451827730b530e73f50203
Tobias Schneider <sct43297@hs-regensburg.de>
2018-07-08 16:26:31 +0200

    Fixed master.c

--- generators/bin/generator/templates/brick/masterc.c
+++ generators/bin/generator/templates/brick/masterc.c
@@ -4,6 +4,13 @@
 
 int localSenderReceiver[<LOCAL_SENDER_RECEIVER_SIZE>];
 int remotePorts[<REMOTE_SENDER_RECEIVER_SIZE>];
+const U8 com_slave_addr[7] = COM_CONNECT_SLAVE_ADDRESS;
+
+#ifdef COM_CONNECT_IS_MASTER
+const U8 btIsmaster = 1;
+#else
+const U8 btIsmaster = 0;
+#endif /* COM_CONNECT_IS_MASTER */
 
 DeclareCounter(CounterOne);
 <DECLARES>
@@ -21,9 +28,15 @@ void user_1ms_isr_type2(void)
 		ShutdownOS(ercd);
 }
 
-#include "../BSW/ComHandler/ComHandler.c"
+#include "../BSW/ADC/AdcIF.h"
+#include "../BSW/ComHandler/ComHandler.h"
+#include "../BSW/DIO/DioIF.h"
+#include "../BSW/DisplayHandler/DisplayHandler.h"
 #include "../BSW/I2C/I2C.h"
+#include "../BSW/MotorHandler/MotorHandler.h"
 #include "../BSW/Sonar/Sonar.h"
+#include "../BSW/SoundHandler/SoundHandler.h"
+#include "../BSW/Utility/Utility.h"
 
 void ecrobot_device_initialize()
 {
