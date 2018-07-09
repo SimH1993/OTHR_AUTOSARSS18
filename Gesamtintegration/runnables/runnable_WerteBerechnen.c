9c3316fec72d1806c3e56c30e359052167224f93
cynt4k <me@cynt4k.de>
2018-07-09 17:38:29 +0200

    Fixed Werte Berechnen

--- Gesamtintegration/runnables/runnable_WerteBerechnen.c
+++ Gesamtintegration/runnables/runnable_WerteBerechnen.c
@@ -6,9 +6,10 @@ S8 speed_right = 0;
 
 void runnable_WerteBerechnen()
 {
-	
-	S8 x = RTE_READ_InJoystickHorizontal();
-	S8 y = RTE_READ_InJoystickVertical();
+	//y=Horizontale
+	//x=Vertikale
+	S8 y = RTE_READ_InJoystickHorizontal();
+	S8 x = RTE_READ_InJoystickVertical();
 	
 	//S8 x = (S8)(data & 0x000000FF);
 	//S8 y = (S8)((data & 0x0000FF00) >> 8);
@@ -16,7 +17,7 @@ void runnable_WerteBerechnen()
 	S8 speed_left = x < 0 ? (y * (100 + x)) / 100 : y;
 	S8 speed_right = x > 0 ? (y * (100 - x)) / 100 : y;
 
-	int data = (speed_right << 8) | speed_left;
+	int data = ((U8)speed_right << 8) | (U8)speed_left;
 	
 	RTE_WRITE_OutMotorspeed(data);
 }
